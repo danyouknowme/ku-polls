f90cbe138bc351863545cec95b9f61072b955273
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-09-11 16:34:53 +0700

    fixed cannot add question

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date ended')
+    end_date = models.DateTimeField('date ended', blank=True, null=True)
 
     def was_published_recently(self):
         """Check the question was published recently
