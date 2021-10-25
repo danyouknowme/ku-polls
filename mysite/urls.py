eba3a7e09ad30fd7a0f8d88fef30a98b2860ad0d
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-25 14:48:11 +0700

    add url for account

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', lambda request: redirect('polls/')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
