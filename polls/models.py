e7429f48156416c6a5cce9f9b183c42ea43f0353
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-27 21:25:57 +0700

    create vote model class

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -52,8 +53,13 @@ class Choice(models.Model):
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
         """Return the content of choice text."""
         return self.choice_text
+
+
+class Vote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, default=0)
